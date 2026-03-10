26bf5850b73aeb3f7b6ba13befa6d7c066b3ffa3
Haixia Cheng <Haixia.Cheng@microsoft.com>
2026-03-10 14:12:24 +0800

    Add bottle declaration to Modernize formula

--- Formula/modernize.rb
+++ Formula/modernize.rb
@@ -22,6 +22,8 @@ class Modernize < Formula
 
   version "0.0.211"
 
+  bottle :unneeded
+
   license :cannot_represent
   # Proprietary — see https://github.com/galiacheng/apmod-cli/blob/HEAD/LICENSE.txt
 
