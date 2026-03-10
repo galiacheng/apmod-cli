d6def17fd3f84556c6bd4291e346b0511c798234
Haixia Cheng <Haixia.Cheng@microsoft.com>
2026-03-10 14:30:52 +0800

    Update Modernize formula to use latest release URLs and remove version specification

--- Formula/modernize.rb
+++ Formula/modernize.rb
@@ -4,26 +4,19 @@ class Modernize < Formula
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/galiacheng/apmod-cli/releases/download/0.0.211/modernize_0.0.211_darwin_x64.tar.gz"
-      sha256 "472e12f37471f25de62431200b7466629a210a9d99854f979bc1c9bf988f1ee8"
+      url "https://github.com/galiacheng/apmod-cli/releases/latest/download/modernize_darwin_x64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/galiacheng/apmod-cli/releases/download/0.0.211/modernize_0.0.211_darwin_arm64.tar.gz"
-      sha256 "04f23f2c57525a851ab270e3ae1c54fd5cfefd261279d711bba6c5e251df89c2"
+      url "https://github.com/galiacheng/apmod-cli/releases/latest/download/modernize_darwin_arm64.tar.gz"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/galiacheng/apmod-cli/releases/download/0.0.211/modernize_0.0.211_linux_x64.tar.gz"
-      sha256 "3b417f9df6cbedc2976655d2ca1ad4f867f49acd8e58ed18c5a1c027912e64cf"
+      url "https://github.com/galiacheng/apmod-cli/releases/latest/download/modernize_linux_x64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/galiacheng/apmod-cli/releases/download/0.0.211/modernize_0.0.211_linux_arm64.tar.gz"
-      sha256 "a587212eb4378f94fc3a4f827aa72f8aa70d97b758b107130a8e7f3562a83dac"
+      url "https://github.com/galiacheng/apmod-cli/releases/latest/download/modernize_linux_arm64.tar.gz"
     end
   end
 
-  version "0.0.211"
-
   license :cannot_represent
-  # Proprietary — see https://github.com/galiacheng/apmod-cli/blob/HEAD/LICENSE.txt
 
   def install
     libexec.install "modernize"
